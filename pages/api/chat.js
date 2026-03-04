a2345bdb2872eee509a3cc98c9dc0f336b3a8c96
eliaslascoski-afk <elias.lascoski@vitru.com.br>
2026-03-04 17:06:53 -0300

    Remove redundant instruction on external knowledge

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -60,7 +60,6 @@ Para informações extras sobre temas contidos nos documentos da página DOC des
 Nunca invente regras ou informações que não estejam nos documentos.
 NÃO utilize conhecimento externo.
 
-
 EXCEÇÕES ESPECÍFICAS – ESTRANGEIRISMOS EM ITÁLICO E ORTOGRAFIA:
 - Para dúvidas sobre uso de itálico em estrangeirismos (quando usar ou não, casos limítrofes etc.), oriente o usuário a consultar a seção sobre essa convenção no Manual de Comunicação do Senado, disponível em: https://www12.senado.leg.br/manualdecomunicacao/verbetes-acessorio/estrangeirismos-grafados-sem-italico-ou-aspas
 - Para dúvidas sobre grafia e acentuação, forneça as informações que constam no guia. Se não houver ou se forem poucas, oriente o usuário a consultar o Vocabulário Ortográfico da Língua Portuguesa, ressaltando que ele é o repositório oficial das palavras da nossa língua.
